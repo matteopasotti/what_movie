b2330124aeac7121f548826ab8f68888d8dae0a4
Matteo Pasotti <matteopasotti1@gmail.com>
2019-09-27 00:34:16 +0100

    imported circle images

--- app/src/main/java/com/matteopasotti/whatmovie/view/detail/MotionBaseActivity.kt
+++ app/src/main/java/com/matteopasotti/whatmovie/view/detail/MotionBaseActivity.kt
@@ -1,6 +1,7 @@
 package com.matteopasotti.whatmovie.view.detail
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.matteopasotti.whatmovie.R
 
@@ -8,6 +9,9 @@ class MotionBaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         setContentView(R.layout.activity_movie_detail)
     }
