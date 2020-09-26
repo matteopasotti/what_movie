832896f26a1b2fdcf363e0ebe919f5a1b3b87707
Matteo Pasotti <matteopasotti1@gmail.com>
2020-09-26 17:19:46 +0100

    rename response

--- app/src/main/java/com/matteopasotti/whatmovie/model/response/RecommendedMovieResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.matteopasotti.whatmovie.model.response
-
-import com.matteopasotti.whatmovie.model.Movie
-
-internal data class RecommendedMovieResponse (
-    val page: Int,
-    val results: List<Movie>?)
