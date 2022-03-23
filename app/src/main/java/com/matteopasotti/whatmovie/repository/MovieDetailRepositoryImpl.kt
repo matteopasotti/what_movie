37b70d27a839e0eb14bb8559d64dc554ad4459b0
Matteo Pasotti <matteopasotti1@gmail.com>
2022-03-23 14:59:09 +0000

    written unit tests for movie detail repository

--- app/src/main/java/com/matteopasotti/whatmovie/repository/MovieDetailRepositoryImpl.kt
+++ app/src/main/java/com/matteopasotti/whatmovie/repository/MovieDetailRepositoryImpl.kt
@@ -42,36 +42,4 @@ internal class MovieDetailRepositoryImpl(
         )
     }
 
-
-//    @Throws(IOException::class)
-//    override suspend fun getSimilarMovies(movieId: Int): List<MovieDomainModel>? {
-//        val response = withContext(Dispatchers.IO) { movieApi.getSimilarMovies(movieId, 1) }
-//        if(response.isSuccessful){
-//            return response.body()?.results?.map { it.toDomainModel() }
-//        }
-//        return null
-//    }
-
-//    @Throws(IOException::class)
-//    override suspend fun getMovieCredits(movieId: Int): List<ActorDomainModel>? {
-//        val response = withContext(Dispatchers.IO) { movieApi.getMovieCredits(movieId) }
-//
-//        if (response.isSuccessful) {
-//            return response.body()?.cast?.filter { it.profile_path != null }?.map { it.toDomainModel() }
-//        }
-//
-//        return null
-//    }
-
-//    @Throws(IOException::class)
-//    override suspend fun getMovieDetail(movieId: Int): MovieDetailDomainModel? {
-//        val response = withContext(Dispatchers.IO) { movieApi.getMovieDetail(movieId) }
-//
-//        if(response.isSuccessful){
-//            return response.body()?.toDomainModel()
-//        }
-//
-//        return null
-//    }
-
 }
--- app/src/test/java/com/matteopasotti/whatmovie/repository/MovieDetailRepositoryImplTest.kt
+++ app/src/test/java/com/matteopasotti/whatmovie/repository/MovieDetailRepositoryImplTest.kt
@@ -1,15 +1,15 @@
 package com.matteopasotti.whatmovie.repository
 
-import com.matteopasotti.whatmovie.BuildConfig
 import com.matteopasotti.whatmovie.DataFixtures
 import com.matteopasotti.whatmovie.api.MovieApiInterface
+import com.matteopasotti.whatmovie.api.Result
 import com.matteopasotti.whatmovie.model.response.BasicMovieResponse
 import com.matteopasotti.whatmovie.model.response.MovieCreditResponse
-import com.matteopasotti.whatmovie.model.toDomainModel
-import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.whenever
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.runBlocking
-import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,149 +21,170 @@ import retrofit2.Response
 class MovieDetailRepositoryImplTest {
 
     @Mock
-    internal lateinit var mockService: MovieApiInterface
+    internal lateinit var movieApi: MovieApiInterface
 
     private lateinit var repository: MovieDetailRepositoryImpl
 
+    private val successResponse =
+        BasicMovieResponse(page = 1, results = listOf(DataFixtures.getMovie()))
+
+    private val errorContent =
+        "{\"status_code\":7,\"status_message\":\"Invalid API key: You must be granted a valid key.\",\"success\":false}"
+
     @Before
     fun setUp() {
-        repository = MovieDetailRepositoryImpl(mockService)
+        repository = MovieDetailRepositoryImpl(movieApi)
     }
 
     @Test
-    fun `getRecommendedMovies fetches Movies and map them to domainModel`() {
+    fun `Given we call getRecommendedMovies,And We get a valid response from Api,Then we return Result Success`() {
         runBlocking {
-            given(mockService.getRecommendedMovies(1, 1))
-                .willReturn(
-                    Response.success(
-                        200, BasicMovieResponse(
-                            page = 1,
-                            results = listOf(DataFixtures.getMovie())
-                        )
-                    )
+
+            whenever(
+                movieApi.getRecommendedMovies(
+                    1, 1
                 )
+            ).thenReturn(
+                Response.success(
+                    200,
+                    successResponse
+                )
+            )
 
-            val result = repository.getRecommendedMovies(1)
+            val expected = Result.Success(successResponse)
+            val actual = repository.getRecommendedMovies(1)
 
-            assertEquals(result!!.first(), DataFixtures.getMovie().toDomainModel())
+            assertEquals(expected, actual)
         }
     }
 
     @Test
-    fun `getRecommendedMovies returns error then we do not return any recommended movie`() {
-        val content =
-            "{\"status_code\":7,\"status_message\":\"Invalid API key: You must be granted a valid key.\",\"success\":false}"
+    fun `Given we call getRecommendedMovies,And we get an Error from Api,Then we return Result Error`() {
         runBlocking {
-            given(mockService.getRecommendedMovies(1, 1))
-                .willReturn(
-                    Response.error(400, ResponseBody.create(null, content))
+            whenever(
+                movieApi.getRecommendedMovies(
+                    1, 1
                 )
+            ).thenReturn(Response.error(400, errorContent.toResponseBody()))
 
-            val result = repository.getRecommendedMovies(1)
+            val actual = repository.getRecommendedMovies(1)
 
-            assertEquals(result, null)
+            Assert.assertTrue(actual is Result.Error)
         }
     }
 
     @Test
-    fun `getSimilarMovies fetches Movies and map them to domainModel`() {
+    fun `Given we call getSimilarMovies,And We get a valid response from Api,Then we return Result Success`() {
         runBlocking {
-            given(mockService.getSimilarMovies(1, 1))
-                .willReturn(
-                    Response.success(
-                        200, BasicMovieResponse(
-                            page = 1,
-                            results = listOf(DataFixtures.getMovie())
-                        )
-                    )
+
+            whenever(
+                movieApi.getSimilarMovies(
+                    1, 1
                 )
+            ).thenReturn(
+                Response.success(
+                    200,
+                    successResponse
+                )
+            )
 
-            val result = repository.getSimilarMovies(1)
+            val expected = Result.Success(successResponse)
+            val actual = repository.getSimilarMovies(1)
 
-            assertEquals(result!!.first(), DataFixtures.getMovie().toDomainModel())
+            assertEquals(expected, actual)
         }
     }
 
     @Test
-    fun `getSimilarMovies returns error then we do not return any similar movie`() {
-        val content =
-            "{\"status_code\":7,\"status_message\":\"Invalid API key: You must be granted a valid key.\",\"success\":false}"
+    fun `Given we call getSimilarMovies,And we get an Error from Api,Then we return Result Error`() {
         runBlocking {
-            given(mockService.getSimilarMovies(1, 1))
-                .willReturn(
-                    Response.error(400, ResponseBody.create(null, content))
+            whenever(
+                movieApi.getSimilarMovies(
+                    1, 1
                 )
+            ).thenReturn(Response.error(400, errorContent.toResponseBody()))
 
-            val result = repository.getSimilarMovies(1)
+            val actual = repository.getSimilarMovies(1)
 
-            assertEquals(result, null)
+            Assert.assertTrue(actual is Result.Error)
         }
     }
 
     @Test
-    fun `getMovieCredits fetches credits and map them to domainModel`() {
+    fun `Given we call getMovieCredits,And We get a valid response from Api,Then we return Result Success`() {
+        val creditResponseSuccess = MovieCreditResponse(11, listOf(DataFixtures.getActor()))
         runBlocking {
-            given(mockService.getMovieCredits(1))
-                .willReturn(
-                    Response.success(200, MovieCreditResponse(
-                        id = 1,
-                        cast = listOf(DataFixtures.getActor())
-                    ))
+            whenever(
+                movieApi.getMovieCredits(
+                    1
                 )
+            ).thenReturn(
+                Response.success(
+                    200,
+                    creditResponseSuccess
+                )
+            )
 
-            val result = repository.getMovieCredits(1)
+            val expected = Result.Success(creditResponseSuccess)
+            val actual = repository.getMovieCredits(1)
 
-            assertEquals(DataFixtures.getActor().toDomainModel(), result!!.first())
+            assertEquals(expected, actual)
         }
     }
 
     @Test
-    fun `getMovieCredits fetches credits and filters out the ones with no profile image`(){
-        val actorWithImage = DataFixtures.getActor(profile_path = "image")
-        val actorWithoutImage = DataFixtures.getActor(profile_path = null)
+    fun `Given we call getMovieCredits,And We get an Error from Api,Then we return Result Error`() {
         runBlocking {
-            given(mockService.getMovieCredits(1))
-                .willReturn(
-                    Response.success(200, MovieCreditResponse(
-                        id = 1,
-                        cast = listOf(actorWithImage, actorWithoutImage)
-                    ))
+            whenever(
+                movieApi.getMovieCredits(
+                    1
                 )
+            ).thenReturn(
+                Response.error(400, errorContent.toResponseBody())
+            )
 
-            val result = repository.getMovieCredits(1)
+            val actual = repository.getMovieCredits(1)
 
-            assertEquals(1, result!!.size)
-            assertEquals(actorWithImage.toDomainModel(), result!!.first())
+            Assert.assertTrue(actual is Result.Error)
         }
     }
 
     @Test
-    fun `getMovieCredits returns error and does not return any actor`(){
-        val content =
-            "{\"status_code\":7,\"status_message\":\"Invalid API key: You must be granted a valid key.\",\"success\":false}"
+    fun `Given we call getMovieDetail,And We get a valid response from Api,Then we return Result Success`() {
+        val movieDetailResponseSuccess = DataFixtures.getMovieDetailResponse()
         runBlocking {
-            given(mockService.getMovieCredits(1))
-                .willReturn(
-                    Response.error(400, ResponseBody.create(null, content))
+            whenever(
+                movieApi.getMovieDetail(
+                    1
                 )
+            ).thenReturn(
+                Response.success(
+                    200,
+                    movieDetailResponseSuccess
+                )
+            )
 
-            val result = repository.getMovieCredits(1)
+            val expected = Result.Success(movieDetailResponseSuccess)
+            val actual = repository.getMovieDetail(1)
 
-            assertEquals(null, result)
+            assertEquals(expected, actual)
         }
     }
 
     @Test
-    fun `getMovieDetail returns movie detail and map it to domainModel`(){
+    fun `Given we call getMovieDetail,And We get an error from Api,Then we return Result Error`() {
         runBlocking {
-            given(mockService.getMovieDetail(1))
-                .willReturn(
-                    Response.success(200, DataFixtures.getMovieDetailResponse())
+            whenever(
+                movieApi.getMovieDetail(
+                    1
                 )
+            ).thenReturn(
+                Response.error(400, errorContent.toResponseBody())
+            )
 
-            val result = repository.getMovieDetail(1)
+            val actual = repository.getMovieDetail(1)
 
-            assertEquals(DataFixtures.getMovieDetailResponse().toDomainModel(), result)
+            Assert.assertTrue(actual is Result.Error)
         }
     }
 }
